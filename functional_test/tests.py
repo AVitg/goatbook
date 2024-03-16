67add727c3e32d8f84365ef62c8f451202f65a3d
AVitg <k-egg2008@gmx.de>
2024-03-16 19:23:17 +0100

    new URL, view and template to display lists

--- functional_test/tests.py
+++ functional_test/tests.py
@@ -103,7 +103,7 @@ class NewVisitorTest(LiveServerTestCase):
         # list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element(By.TAG_NAME,"body").text
-        self.assertNotIn("Buy peacok feathers", page_text)
+        self.assertNotIn("Buy peacock feathers", page_text)
         self.assertNotIn("make a fly",page_text)
         
         # Francis starts a new list by entering a new item. He
--- lists/tests.py
+++ lists/tests.py
@@ -7,32 +7,34 @@ from lists.models import Item
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get("/")  
-        
-        self.assertContains(response,"<title>To-Do lists</title>")
-        self.assertContains(response,"<html>")
-        self.assertContains(response,"</html>")
-        
-        self.assertContains(response, "<title>To-Do lists</title>") 
         self.assertTemplateUsed(response, "home.html")  
-
-    def test_displays_all_list_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
-        response = self.client.get("/")
-        self.assertContains(response, "itemey 1")
-        self.assertContains(response, "itemey 2")
-
 
     def test_can_save_a_POST_request(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, "A new list item")
-         
-    def test_redirect_after_POST(self):    
-        response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertRedirects(response, "/")  
 
+    def test_redirects_after_POST(self):
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
+
+class ListViewTest(TestCase):    
+    def test_uses_list_template(self):
+        response = self.client.get("/lists/the-only-list-in-the-world/")
+        self.assertTemplateUsed(response, "list.html")
+
+    
+    
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+        response = self.client.get("/lists/the-only-list-in-the-world/")
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
+
+
+         
             
     def test_only_saves_items_when_necessary(self):
         self.client.get("/")
