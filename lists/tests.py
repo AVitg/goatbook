f8073b0bffb1032bd75dbf1b6ecda5cc70704748
AVitg <k-egg2008@gmx.de>
2024-04-05 21:37:05 +0200

    Resti ified

--- lists/tests.py
+++ lists/tests.py
@@ -43,7 +43,6 @@ class NewListTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post("/lists/new", data={"item_text": "A new list item"})
         self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
-
     
 
 class ItemModelTest(TestCase):
